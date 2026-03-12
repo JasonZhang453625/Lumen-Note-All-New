045d68ed8b2b7a9c5507da578d795921e09d5d99
Jason <jd4688210@gmail.com>
2026-03-12 12:09:02 +0800

    Dev1.2.3

--- android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_application_1/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.example.flutter_application_1
 
+import android.os.Build
+import android.view.Display
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        requestHighRefreshRate()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestHighRefreshRate()
+    }
+
+    private fun requestHighRefreshRate() {
+        val currentDisplay = currentDisplay() ?: return
+        val highestMode = currentDisplay.supportedModes.maxWithOrNull(
+            compareBy<Display.Mode>({ it.refreshRate }, { it.physicalWidth * it.physicalHeight }),
+        ) ?: return
+
+        val params = window.attributes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            params.preferredDisplayModeId = highestMode.modeId
+        }
+        params.preferredRefreshRate = highestMode.refreshRate
+        window.attributes = params
+    }
+
+    private fun currentDisplay(): Display? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            display
+        } else {
+            @Suppress("DEPRECATION")
+            windowManager.defaultDisplay
+        }
+    }
+}
